eca1038003d971e7122c50451744be3f3d931e80
utsav <utsavdoris@gmail.com>
2026-01-22 10:10:06 +0530

    update

--- ring-designer/app/ring-data.ts
+++ ring-designer/app/ring-data.ts
@@ -6,31 +6,31 @@ export const DEFAULT_HEAD_URL =
 export const SHANK_DATA = [
   {
     style: "Solitaire Shank",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FCLASSIC-SOLITAIRE-SHANK.glb?alt=media&token=7c8b8168-5cc6-4612-9d77-e3f44ba9b5a2",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FCLASSIC-SOLITAIRE-SHANK.glb?alt=media&token=1bf5dd03-bdde-440d-a022-5a5ac72c440b",
   },
   {
     style: "3-row Pave Diamond",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2F3-Row-Pawe-Diamond-Shank.glb?alt=media&token=6f13506f-7927-4e03-838d-38945af39252",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2F3-Row-Pawe-Diamond-Shank.glb?alt=media&token=16dc4011-7239-498c-9c1e-de29805834ee",
   },
   {
     style: "Double Row Diamond",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FDouble%20Row%20Diamond%20shank.glb?alt=media&token=ddc2955f-af3d-4992-8fc6-104ba3fec94f",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FDouble%20Row%20Diamond%20shank.glb?alt=media&token=34ca2762-73b2-46c2-9e3b-b7920abe930e",
   },
   {
     style: "Split",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FSPLIT%20SHANK.glb?alt=media&token=ed5890ff-1573-4eb5-8ea8-55d6c3e487f5",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FSPLIT%20SHANK.glb?alt=media&token=c8c1ba59-e27f-49b2-8a9e-91888620edec",
   },
   {
     style: "Garden",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FGARDEN%20SHANK.glb?alt=media&token=60fa5528-3d77-460a-9f0e-563c1fb1e2ce",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FGARDEN%20SHANK.glb?alt=media&token=7b262f61-0a23-4d12-b44a-627ccedc4c31",
   },
   {
     style: "Single Row Diamond",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FSingle%20Row%20Diamond%20Shank.glb?alt=media&token=9419f7ba-9e06-49ef-933c-e90c4d4df162",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FSingle%20Row%20Diamond%20Shank.glb?alt=media&token=9cf187f9-838b-4a49-bcb0-d0acb604085d",
   },
   {
     style: "Twist",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fshank%2FTWIST%20SHANK.glb?alt=media&token=3c7aaba3-4483-49c1-a573-acd5c92474c3",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fshank%2FTWIST%20SHANK.glb?alt=media&token=12b6a9d9-7955-49da-b545-66a620e9d86f",
   },
 ];
 
@@ -39,52 +39,52 @@ export const HEAD_DATA = [
   {
     setting: "Hidden Halo",
     shape: "Asscher",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN_HALO_ASSCHER_1.5CT.glb?alt=media&token=59ef4628-6c15-4148-8b99-e86c8da4871e",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN_HALO_ASSCHER_1.5CT.glb?alt=media&token=cf69f8f1-6e48-4ef5-8ff5-96fe739a9382",
   },
   {
     setting: "Hidden Halo",
     shape: "Heart",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN_HALO_HEART_1.5CT.glb?alt=media&token=570c69c1-7aa8-416e-bd2d-a06648f1572c",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN_HALO_HEART_1.5CT.glb?alt=media&token=9d81ff6b-032e-4f1d-af9e-b227d11d0496",
   },
   {
     setting: "Hidden Halo",
     shape: "Cushion",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN_HALO_CUSHION_3CT.glb?alt=media&token=66c0cbb7-3b8f-4748-b5a4-abb6bddc4045",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN_HALO_CUSHION_3CT.glb?alt=media&token=ac46e7f0-ddfb-411c-ab4a-e94cb0bb1254",
   },
   {
     setting: "Hidden Halo",
     shape: "Emerald",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_EMERALD_1.5CT.glb?alt=media&token=4969d247-df65-48e7-b229-f99b9025dbf8",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_EMERALD_1.5CT.glb?alt=media&token=ae8a7629-d5c1-48eb-90a6-202f35ac4ac1",
   },
   {
     setting: "Hidden Halo",
     shape: "Marquise",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_MARQUISE_1.5CT.glb?alt=media&token=bbcc0050-6e86-4ee1-a48e-357948519af1",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_MARQUISE_1.5CT.glb?alt=media&token=884e1dc1-72cf-447c-aa8c-dd9336931a21",
   },
   {
     setting: "Hidden Halo",
     shape: "Oval",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_OVAL_1.5CT.glb?alt=media&token=52b5ca09-2a5b-49a1-85b4-bed17361549c",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_OVAL_1.5CT.glb?alt=media&token=b16b66f1-32a9-4cfa-9c62-2e9a9d39c134",
   },
   {
     setting: "Hidden Halo",
     shape: "Pear",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_PEAR_1.5CT.glb?alt=media&token=3b527d6f-9425-44c8-90d2-9eb2fc7dff57",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_PEAR_1.5CT.glb?alt=media&token=7a3cabbf-0a6a-494c-a19a-066c66e50457",
   },
   {
     setting: "Hidden Halo",
     shape: "Princess",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_PRINCESS_1.5CT.glb?alt=media&token=c6b58f6e-1849-48ee-b56a-50cb4ff073dc",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_PRINCESS_1.5CT.glb?alt=media&token=46f43e2c-dab9-4714-898d-c16c20ec36cd",
   },
   {
     setting: "Hidden Halo",
     shape: "Radiant",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_RADIANT_1.5CT.glb?alt=media&token=809564fa-98ab-40aa-a355-d8cbb287aa1e",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_RADIANT_1.5CT.glb?alt=media&token=495254fd-625d-47e5-ae37-b400c6b0ee83",
   },
   {
     setting: "Hidden Halo",
     shape: "Round",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FHIDDEN%20HALO_ROUND_1.5CT.glb?alt=media&token=05fe36d5-582b-4060-a091-1dc3f3c1f193",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FHIDDEN%20HALO_ROUND_1.5CT.glb?alt=media&token=b99782ce-f416-4201-a036-2024b92dbbc6",
   },
 
   // Basket Set
@@ -93,6 +93,6 @@ export const HEAD_DATA = [
   {
     setting: "Basket Set",
     shape: "Oval",
-    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/testing-ring-builder%2Fhead%2FBASKET_SET_OVAL_3CT.glb?alt=media&token=e4de6cb8-dd2e-4352-b3f1-9f382372d074",
+    url: "https://firebasestorage.googleapis.com/v0/b/qa-ultimate-jewelry.firebasestorage.app/o/compress-ring-builder%2Fhead%2FBASKET_SET_OVAL_3CT.glb?alt=media&token=a6ff525b-21e2-434e-923d-c947b98a6361",
   },
 ];
